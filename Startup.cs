ac54c9f5b9b7c3a07d8a597535bca8c09b7df449
bommuraja <rbommuraja@outlook.com>
2022-11-06 18:42:05 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shares.Startup))]
+namespace Shares
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
